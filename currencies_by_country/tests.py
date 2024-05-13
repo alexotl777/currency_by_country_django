429af946db3ca0265bd0220f85b86cc0fed05b7d
Отливан Дмитрий Алексеевич <daotlivan@edu.hse.ru>
2024-05-14 01:40:40 +0300

    Init readme, первый тест в tests.py

--- currencies_by_country/tests.py
+++ currencies_by_country/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from unittest.mock import patch
+import json
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_redirect_to_main(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
